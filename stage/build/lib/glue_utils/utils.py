ae9ba5b2c01ecb96c8187cf9d2693f6171fba8b7
carlos sarmiento <csarmiento@caleidos.pe>
2024-07-29 03:46:05 -0500

    Agregando notebooks de prueba

---
+++ stage/build/lib/glue_utils/utils.py
@@ -0,0 +1,57 @@
+import boto3, json
+from awsglue import DynamicFrame
+from pyspark.sql import Window
+from pyspark.sql.types import *
+from pyspark.sql.functions import col, row_number, col, row_number
+
+def get_dyf_from_s3(glueContext, source_config, format='csv'):
+    cnn_options = {
+        "paths": [source_config['s3_path'] + source_config['source'] + "/"], 
+        "recurse": True,
+        "compression": "gzip"
+    }
+    if ('groupFiles' in source_config):
+        cnn_options['groupFiles'] = source_config['groupFiles']
+    if ('groupSize' in source_config):
+        cnn_options['groupSize'] = source_config['groupSize']
+    if ('useS3ListImplementation' in source_config):
+        cnn_options['useS3ListImplementation'] = source_config['useS3ListImplementation']
+    return glueContext.create_dynamic_frame.from_options( \
+            connection_type="s3", \
+            connection_options=cnn_options, \
+            format=format, \
+            format_options={"withHeader": True})
+
+def read_json_from_s3(bucket_name, path):
+    s3 = boto3.resource('s3')
+    content_object = s3.Object(bucket_name, path)
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    return json.loads(file_content)
+
+
+def transform(glueContext, source_config, config, source_inclussions = None):
+    dyf_map = {}
+    for el_name, el_config in config.items():
+        if el_name in source_inclussions:
+            source = el_config["source"]
+            destination = el_config["destination"]
+            mappings = el_config["mappings"]
+            autoincrement_column = el_config["autoincrement_column"]
+
+            # Read CSV from the source path
+            if source_config['type'] == 's3':
+                dyf = get_dyf_from_s3(glueContext, source_config)
+
+            # Apply column renaming and data type transformations
+            dyf_schema = dyf.schema()
+            dyf_t = dyf.apply_mapping(list(map(lambda t: (t[0], dyf_schema.getField(t[0]).dataType.typeName(), t[2], t[3]) if t[1] == "Dynamic" else tuple(t), mappings)))
+            if autoincrement_column:
+                autoincrement_order_by = el_config["autoincrement_order_by"]
+                window_spec_id = Window.orderBy(autoincrement_order_by)
+                df_t = dyf_t.toDF().withColumn(autoincrement_column, row_number().over(window_spec_id))\
+                        .withColumn(autoincrement_column, col(autoincrement_column))
+                
+                dyf_t = DynamicFrame.fromDF(df_t, glueContext, "dyf_t")
+            
+            dyf_map[el_name] = {'table': destination, 'data': dyf_t}
+    return dyf_map
